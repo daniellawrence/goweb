b732ffb7e95893f1833409a975743abad6df440a
Daniel Lawrence <dannyla@linux.com>
2014-07-14 21:58:17 +1000

    Added simple read from filesystem

--- web.go
+++ web.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io/ioutil"
+	
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler_simple(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi\n")
 }
 
+func handler_readfile(w http.ResponseWriter, r *http.Request) {
+	data, _ := ioutil.ReadFile("./web.go")
+	fmt.Fprintf(w, string(data))
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler_simple)
+	http.HandleFunc("/file", handler_readfile)
 	http.ListenAndServe(":8080", nil)
 }
