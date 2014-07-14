9e55820b111b7742b47c8759609c734d66e7a4b0
Daniel Lawrence <dannyla@linux.com>
2014-07-14 22:20:33 +1000

    added request for /big data

--- web.go
+++ web.go
@@ -15,9 +15,17 @@ func handler_readfile(w http.ResponseWriter, r *http.Request) {
 	data, _ := ioutil.ReadFile("./web.go")
 	fmt.Fprintf(w, string(data))
 }
+func handler_bigresponse(w http.ResponseWriter, r *http.Request) {
+	bytes := make([]byte, 5000)
+	for i, _ := range bytes {
+		bytes[i] = 'a'
+	}
+	fmt.Fprintf(w, string(bytes))
+}
 
 func main() {
 	http.HandleFunc("/", handler_simple)
 	http.HandleFunc("/file", handler_readfile)
+	http.HandleFunc("/big", handler_bigresponse)
 	http.ListenAndServe(":8080", nil)
 }
